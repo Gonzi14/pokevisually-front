692304a67dc514ffb90079612bd48293492d228a
Gonzi14 <gonzaloandrade142@gmail.com>
2024-01-27 09:37:43 -0300

    added generation button

--- src/components/NextButton/NextButton.jsx
+++ src/components/NextButton/NextButton.jsx
@@ -3,7 +3,6 @@ import "./NextButton.css";
 export const NextButton = ({ icon, handleClick }) => {
   return (
     <button className="buttons" onClick={handleClick}>
-      {" "}
       {icon}
     </button>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,18 @@ import { TiArrowLeftOutline, TiArrowRightOutline } from "react-icons/ti";
 
 const CANT_POKEMON_MIN = 152;
 const CANT_POKEMON_MAX = 251;
+const generations = [
+  { id: 1, min: 1, max: 151 },
+  { id: 2, min: 152, max: 251 },
+  { id: 3, min: 252, max: 386 },
+  { id: 4, min: 387, max: 493 },
+  { id: 5, min: 494, max: 649 },
+  { id: 6, min: 650, max: 721 },
+  { id: 7, min: 722, max: 809 },
+  { id: 8, min: 810, max: 905 },
+  { id: 9, min: 906, max: 1010 },
+  { id: 0, min: 1, max: 1010 },
+];
 //primera generacion: 1-151
 //segunda generacion: 152-251
 //tercera generacion: 252-386
@@ -16,21 +28,21 @@ const CANT_POKEMON_MAX = 251;
 //septima generacion: 722-809
 //octava  generacion: 810-905
 //novena  generacion: 906-1010
-
-const getRandomNumber = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+const getRandomNumber = (currentGeneration) => {
+  return Math.floor(
+    Math.random() * (currentGeneration.max - currentGeneration.min + 1) +
+      currentGeneration.min
+  );
 };
 
 export const Home = () => {
+  const [currentGeneration, setCurrentGeneration] = useState(generations[0]);
   const [currentPokemon, setCurrentPokemon] = useState(null);
   const [pokemonsId, setCurrentPokemonId] = useState([
-    getRandomNumber(CANT_POKEMON_MIN, CANT_POKEMON_MAX),
+    getRandomNumber(currentGeneration),
   ]);
   useEffect(() => {
-    setCurrentPokemonId([
-      ...pokemonsId,
-      getRandomNumber(CANT_POKEMON_MIN, CANT_POKEMON_MAX),
-    ]);
+    setCurrentPokemonId([...pokemonsId, getRandomNumber(currentGeneration)]);
     getPokemonFromAPI(pokemonsId[pokemonsId.length - 1]).then((pokemon) => {
       setCurrentPokemon(pokemon);
     });
@@ -43,7 +55,12 @@ export const Home = () => {
     <>
       <div>
         <h1>Pokeggstats!</h1>
-
+        <NextButton
+          icon={currentGeneration.id}
+          handleClick={async () => {
+            setCurrentGeneration(generations[currentGeneration.id]);
+          }}
+        />
         <PokemonCard pokemon={currentPokemon} />
         <div className="parent">
           <NextButton
@@ -61,7 +78,7 @@ export const Home = () => {
             handleClick={async () => {
               setCurrentPokemonId([
                 ...pokemonsId,
-                getRandomNumber(CANT_POKEMON_MIN, CANT_POKEMON_MAX),
+                getRandomNumber(currentGeneration),
               ]);
               console.log(pokemonsId);
               setCurrentPokemon(
