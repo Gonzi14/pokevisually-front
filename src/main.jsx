4230007669befa760644c4252ae2bbcfac5eb0dd
gbbelloponce <gbbelloponce@gmail.com>
2024-01-27 23:09:02 -0300

    Append app to root

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import { Home } from "./pages/Home/Home";
 import "./index.css";
 
-ReactDOM.createRoot(document.body).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>
