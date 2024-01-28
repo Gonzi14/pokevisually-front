1d27c59571ce4c0b4ee80723094181ba57976668
gbbelloponce <gbbelloponce@gmail.com>
2024-01-27 23:09:41 -0300

    Refactor + fix next pokemon bugs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,9 +5,7 @@ import "./Home.css";
 import { NextButton } from "../../components/NextButton/NextButton";
 import { TiArrowLeftOutline, TiArrowRightOutline } from "react-icons/ti";
 
-const CANT_POKEMON_MIN = 152;
-const CANT_POKEMON_MAX = 251;
-const generations = [
+const GENERATIONS = [
   { id: 1, min: 1, max: 151 },
   { id: 2, min: 152, max: 251 },
   { id: 3, min: 252, max: 386 },
@@ -28,66 +26,78 @@ const generations = [
 //septima generacion: 722-809
 //octava  generacion: 810-905
 //novena  generacion: 906-1010
-const getRandomNumber = (currentGeneration) => {
-  return Math.floor(
-    Math.random() * (currentGeneration.max - currentGeneration.min + 1) +
-      currentGeneration.min
-  );
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 export const Home = () => {
-  const [currentGeneration, setCurrentGeneration] = useState(generations[0]);
+  const [currentGeneration, setCurrentGeneration] = useState(GENERATIONS[0]);
   const [currentPokemon, setCurrentPokemon] = useState(null);
-  const [pokemonsId, setCurrentPokemonId] = useState([
-    getRandomNumber(currentGeneration),
-  ]);
+  const [seenPokemon, setSeenPokemon] = useState([]);
+
   useEffect(() => {
-    setCurrentPokemonId([...pokemonsId, getRandomNumber(currentGeneration)]);
-    getPokemonFromAPI(pokemonsId[pokemonsId.length - 1]).then((pokemon) => {
-      setCurrentPokemon(pokemon);
-    });
+    updatePokemon();
   }, []);
+
+  const updatePokemon = async (pokedexNumber) => {
+    if (pokedexNumber) {
+      const pokemon = await getPokemonFromAPI(pokedexNumber);
+      setCurrentPokemon(pokemon);
+    } else {
+      const pokedexNumber = getNewPokedexNumber();
+      const pokemon = await getPokemonFromAPI(pokedexNumber);
+      setCurrentPokemon(pokemon);
+      setSeenPokemon((prev) => [...prev, pokedexNumber]);
+    }
+  };
+
+  const getNewPokedexNumber = () => {
+    while (true) {
+      const newPokedexNumber = getRandomNumber(
+        currentGeneration.min,
+        currentGeneration.max
+      );
+      if (!seenPokemon.includes(newPokedexNumber)) {
+        return newPokedexNumber;
+      }
+    }
+  };
+
+  const getPreviousSeenPokemon = () => {
+    const currentPosition = seenPokemon.indexOf(currentPokemon.pokedexNumber)
+    return seenPokemon.at(currentPosition - 1);
+  };
 
   if (!currentPokemon) {
     return <div>Cargando Contenido...</div>;
   }
   return (
-    <>
-      <div>
-        <h1>Pokeggstats!</h1>
+    <div>
+      <h1>Pokeggstats!</h1>
+      <NextButton
+        icon={currentGeneration.id}
+        handleClick={async () => {
+          setCurrentGeneration(GENERATIONS[currentGeneration.id]);
+        }}
+      />
+      <PokemonCard pokemon={currentPokemon} />
+      <div className="parent">
         <NextButton
-          icon={currentGeneration.id}
+          icon={<TiArrowLeftOutline />}
           handleClick={async () => {
-            setCurrentGeneration(generations[currentGeneration.id]);
+            setCurrentPokemon(
+              await getPokemonFromAPI(getPreviousSeenPokemon())
+            );
           }}
         />
-        <PokemonCard pokemon={currentPokemon} />
-        <div className="parent">
-          <NextButton
-            icon={<TiArrowLeftOutline />}
-            handleClick={async () => {
-              setCurrentPokemon(
-                // cuando haces uno a la derecha, uno a la izquierda y de  vuelta a la derecha, hay un error
-                // ver como resolverlo
-                await getPokemonFromAPI(pokemonsId[pokemonsId.length - 3])
-              );
-            }}
-          />
-          <NextButton
-            icon={<TiArrowRightOutline />}
-            handleClick={async () => {
-              setCurrentPokemonId([
-                ...pokemonsId,
-                getRandomNumber(currentGeneration),
-              ]);
-              console.log(pokemonsId);
-              setCurrentPokemon(
-                await getPokemonFromAPI(pokemonsId[pokemonsId.length - 1])
-              );
-            }}
-          />
-        </div>
+        <NextButton
+          icon={<TiArrowRightOutline />}
+          handleClick={() => {
+            const currentPosition = seenPokemon.indexOf(currentPokemon.pokedexNumber);
+            updatePokemon(seenPokemon.at(currentPosition + 1));
+          }}
+        />
       </div>
-    </>
+    </div>
   );
 };
